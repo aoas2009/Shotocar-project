c70b2f51f35402fb4d79b3d799c9c87987a33b5b
meguito <doasilva2009@hotmail.com>
2026-03-29 18:44:14 -0300

    feat: adding pin component

--- frontend/src/components/PIN.jsx
+++ frontend/src/components/PIN.jsx
@@ -3,20 +3,30 @@ import "./PIN.css";
 
 function LockScreen() {
   return (
-    <div className="flex gap-1">
-      <InputBox />
-      <InputBox />
-      <InputBox />
-      <p>-</p>
-      <InputBox />
-      <InputBox />
-      <InputBox />
+    <div className="flex gap-2 justify-stretch items-center">
+      <div className="flex gap-x-3">
+        <InputBox />
+        <InputBox />
+        <InputBox />
+      </div>
+      <div className="bg-(--gray-2) min-h-1.25 w-4 rounded-2xl"></div>
+      <div className="flex gap-x-3">
+        <InputBox />
+        <InputBox />
+        <InputBox />
+      </div>
     </div>
   );
 }
 
 function InputBox() {
-  return <input type="number" maxLength={1} className="bg-(--black-"/>;
+  return (
+    <input
+      type="text"
+      maxLength={1}
+      className="min-w-0 max-h-12 rounded-lg aspect-square bg-(--gray) border-3 border-(--gray-2) text-(--white-2)! text-center font-bold"
+    />
+  );
 }
 
 export default LockScreen;

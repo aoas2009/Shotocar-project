09d8f786df7103537b24f59e75d95204792820f5
meguito <doasilva2009@hotmail.com>
2026-04-01 07:51:48 -0300

    feat: adding icons in assets/icons

--- frontend/src/components/Button.jsx
+++ frontend/src/components/Button.jsx
@@ -1,12 +1,20 @@
 import { useState } from "react";
 import "./Button.css";
 
-function Button() {
+function Button({ text, color, icon, reverse }) {
   return (
-    <button>
-      
+    <button className={(reverse) ? "flex-row-reverse" : "flex-row" + "justify-center items-center" + "bg-(--" + color + ")" }>
+      {getIcon(icon)}
+      <span>{text}</span>
     </button>
   );
+}
+
+function getIcon(icon) {
+  switch (icon) {
+    "atualizar":
+      return ()
+  }
 }
 
 export default Button;

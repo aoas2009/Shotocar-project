1522c7525191c24041a35f9234ab82c5af73186e
meguito <doasilva2009@hotmail.com>
2026-04-08 09:35:34 -0300

    feat: structuring lockscreen layout

--- frontend/src/components/Button.jsx
+++ frontend/src/components/Button.jsx
@@ -4,12 +4,12 @@ import { Component } from "react";
 import Icon from "./Icon";
 import { MissingPropError } from "@/js/error";
 
-function Button({ text, color = "gray", icon, reverse = false, classes }) {
+function Button({ text, color = "gray", icon, reverse = false, size = "w-full", classes }) {
   if (!text) throw new MissingPropError("text", "Button")
 
   const flow = (reverse) ? "flex-row" : "flex-row-reverse";
   return (
-    <button className={`flex ${flow} justify-center items-center bg-(--${color}) p-2 gap-1 rounded-md border-3 border-(--gray-2) ${classes || ""}`}>
+    <button className={`flex ${flow} justify-center items-center bg-(--${color}) p-2 gap-1 rounded-md border-3 border-(--gray-2) ${size} ${classes || ""}`}>
       {icon && <Icon icon={icon} size={"small"} />}
       <span className="">{text}</span>
     </button>
--- frontend/src/pages/LockScreen.jsx
+++ frontend/src/pages/LockScreen.jsx
@@ -6,17 +6,26 @@ import Button from "../components/Button";
 
 function LockScreen() {
   return (
-    <>
-      <div></div>
-      <div>
+    <div className="flex">
+      <div className="min-w-1/3">
+        <p>Olao</p>
+      </div>
+      <div className="flex-col flex-1 h-lvh justify-center items-center">
         <div>
-          <h1>Olá!</h1>
+          <h1 className="font-bold text-4xl">Olá, Shoto</h1>
+          <p className="sub-text font-semibold text-2xl">
+            Insira seu PIN para prosseguir
+          </p>
           <PIN></PIN>
         </div>
+        <Button
+          text={"Relembrar PIN"}
+          icon={"paper-plane"}
+          classes={"w-full"}
+          size={"max-w-1/4"}
+        />
       </div>
-
-      <Button text={"Relembrar PIN"} icon={"paper-plane"} classes={"w-full"} />
-    </>
+    </div>
   );
 }
 
